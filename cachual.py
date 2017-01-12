230b0fbc8c8774d3b5c8e5d3139cdb0c7874df8c
Alex Landau <b.alex.landau@gmail.com>
2017-01-12 01:24:26 -0800

    Bump version to 0.2.1

--- cachual.py
+++ cachual.py
@@ -9,7 +9,7 @@ from functools import wraps
 from redis import StrictRedis
 from pymemcache.client.base import Client as MemcachedClient
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 class CachualCache(object):
     """Base class for all cache implementations. Provides the
